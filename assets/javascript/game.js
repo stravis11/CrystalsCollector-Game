9533ec3cb69ad22c3dae2937a471b8bc921d5b3f
Steve Travis <stevetravis11@gmail.com>
2019-03-18 13:17:15 -0400

    wip: Got loss condition working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,12 @@ $(document).ready(function () {
 
 
     //Generate random number between 19 - 120 for number to guess
-    var random = Math.floor(Math.random() * 120) + 19;
-    console.log(random);
-    var targetNumb = random;
+    var randomNum = Math.floor(Math.random() * 120) + 19;
+    console.log(randomNum);
+
 
     //Display random number in Number to guess field
-    $("#num-to-guess").html(targetNumb);
+    $("#num-to-guess").html(randomNum);
 
     //Generate random numbers betwee 1 - 12 for crystals
     var crystalOne = Math.floor(Math.random() * 12) + 1;
@@ -46,37 +46,46 @@ $(document).ready(function () {
         update(crystalFour);
     });
 
-//Updating crystal values and total score function
-function update(crystal) {
+    //Updating crystal values and total score function
+    function update(crystal) {
 
-    //Add totalScore and crystal parameters together
-    totalScore += crystal;
-    //Remove previous entries
-    $("#totalScore").empty();
-    //Add player's totalScore value
-    $("#totalScore").append(totalScore);
-    
-    console.log(totalScore);
+        //Add totalScore and crystal parameters together
+        totalScore += crystal;
+        //Remove previous entries
+        $("#totalScore").empty();
+        //Add player's totalScore value
+        $("#totalScore").append(totalScore);
 
-};
-
-//Rest game function
-function resetGame() {
-
-    //Generate a new total score number
-    random = Math.floor(Math.random() * 120) + 19;
-    targetNumb = random;
-
-    //Genreate new random numbers for crystals
-    crys1Num = Math.floor(Math.random() * 12) + 1;
-    crys2Num = Math.floor(Math.random() * 12) + 1;
-    crys3Num = Math.floor(Math.random() * 12) + 1;
-    crys4Num = Math.floor(Math.random() * 12) + 1;
-
-    //Reset total score back to zero
-    totalScore = 0;
-    $("#totalScore").html(totalScore);
-};
+        console.log(totalScore);
 
 
-});
+
+        //Loss condition
+        if (totalScore > randomNum) {
+            //Increment losses by 1
+            losses++;
+            //Display losses on page
+            $("#losses").html(losses);
+        }
+
+    };
+
+        //Rest game function
+        function resetGame() {
+
+            //Generate a new total score number
+            randomNum = Math.floor(Math.random() * 120) + 19;
+
+            //Genreate new random numbers for crystals
+            crys1Num = Math.floor(Math.random() * 12) + 1;
+            crys2Num = Math.floor(Math.random() * 12) + 1;
+            crys3Num = Math.floor(Math.random() * 12) + 1;
+            crys4Num = Math.floor(Math.random() * 12) + 1;
+
+            //Reset total score back to zero
+            totalScore = 0;
+            $("#totalScore").html(totalScore);
+        };
+
+
+    });
